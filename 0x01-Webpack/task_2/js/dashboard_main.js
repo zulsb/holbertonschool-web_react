f015739b11ba86b65233f2a78608d57bf34a0739
zulsb <zulsb2093@gmail.com>
2021-02-13 00:03:29 -0500

    Fix

--- 0x01-Webpack/task_2/js/dashboard_main.js
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import _ from 'lodash';
 import '../css/main.css';
+$('body').append('<div id="logo"></div>')
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
