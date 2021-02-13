2fda0661241763f33dd2c26d4539144e38c032a1
zulsb <zulsb2093@gmail.com>
2021-02-12 23:36:09 -0500

    Adding CSS & Images

---
+++ 0x01-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,14 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import '../css/main.css';
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+let track = 0;
+function updateCounter() {
+  track++;
+  $('#count').html(`${track} clicks on the button`);
+};
+$('button').on('click', _.debounce(updateCounter));
