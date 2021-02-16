5acafb1804f8a5688d45dbc12dd56e87b5a0ddc8
zulsb <zulsb2093@gmail.com>
2021-02-16 00:57:35 -0500

    updatecounter

--- 0x01-Webpack/task_3/modules/body/body.js
+++ 0x01-Webpack/task_3/modules/body/body.js
@@ -1,16 +1,13 @@
-import $ from 'jquery'
+import $ from 'jquery';
 import _ from 'lodash';
-import './body.css'
+import './body.css';
 
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
-
 let track = 0;
-
-const updateCounter = () => {
-    track++;
-    $('#count').html(`${track} clicks on the button`);
+function updateCounter() {
+  track++;
+  $('#count').html(`${track} clicks on the button`);
 };
-
-$('button').on('click', _.debounce(updateCounter));
+$('button').on('click', _.debounce(updateCounter, 500));
