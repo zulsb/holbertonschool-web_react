6ebf7ac5184d593efca4df5dd13dec7a5e0247ba
zulsb <zulsb2093@gmail.com>
2021-02-15 23:34:24 -0500

    Dev servers, modules, and tree shaking

--- 0x01-Webpack/task_3/modules/body/body.js
+++ 0x01-Webpack/task_3/modules/body/body.js
@@ -6,8 +6,8 @@ $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 let track = 0;
-function updateCounter() {
+const updateCounter = () => {
   track++;
   $('#count').html(`${track} clicks on the button`);
 };
-$('button').on('click', _.debounce(updateCounter, 500));
+$('button').on('click', _.debounce(updateCounter));
