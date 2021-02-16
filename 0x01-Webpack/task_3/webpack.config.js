1e7136bae1ff67b2bc655343cbe2d9487bb03d50
zulsb <zulsb2093@gmail.com>
2021-02-15 23:52:03 -0500

    Comments delete

--- 0x01-Webpack/task_3/webpack.config.js
+++ 0x01-Webpack/task_3/webpack.config.js
@@ -5,10 +5,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    all: ["./modules/header/header.js", "./modules/body/body.js", "./modules/footer/footer.js"],
-    /*header: './modules/header/header.js',
-    body: './modules/body/body.js',
-    footer: './modules/footer/footer.js'*/
+    all: ["./modules/header/header.js", "./modules/body/body.js", "./modules/footer/footer.js"] 
   },
   performance: {
     maxAssetSize: 1000000,
