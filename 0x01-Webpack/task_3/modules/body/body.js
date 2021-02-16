3291d0b169cf77e8d08b78f6cf3ff697d124da59
zulsb <zulsb2093@gmail.com>
2021-02-15 23:33:03 -0500

    Dev servers, modules, and tree shaking

--- 0x01-Webpack/task_3/modules/body/body.js
+++ 0x01-Webpack/task_3/modules/body/body.js
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 import _ from 'lodash';
-import '../css/body.css';
+import './body.css';
 
 $('body').append('<p>Dashboard data for the students</p>');
 $('body').append('<button>Click here to get started</button>');
