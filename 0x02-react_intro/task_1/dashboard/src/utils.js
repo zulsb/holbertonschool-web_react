6dbff5aa61fcd29a9e35e8d72a928d3401276d4d
zulsb <zulsb2093@gmail.com>
2021-02-17 03:08:48 -0500

    Embedding expressions, functions

---
+++ 0x02-react_intro/task_1/dashboard/src/utils.js
@@ -0,0 +1,7 @@
+export function getFullYear() {
+  return new Date().getFullYear();
+}
+export function getFooterCopy(isIndex) {
+  if (isIndex) return "Holberton School"
+  return "Holberton School main dashboard"
+}
