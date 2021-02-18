1b6240d94f8f14bfcae64930aaa3ad1d15b4de53
zulsb <zulsb2093@gmail.com>
2021-02-17 20:04:18 -0500

    Modify the Notifications

--- 0x02-react_intro/task_2/dashboard/src/utils.js
+++ 0x02-react_intro/task_2/dashboard/src/utils.js
@@ -5,3 +5,6 @@ export function getFooterCopy(isIndex) {
   if (isIndex) return "Holberton School"
   return "Holberton School main dashboard"
 }
+export function getLatestNotification() {
+  return "<strong>Urgent requirement</strong> - complete by EOD"
+}
