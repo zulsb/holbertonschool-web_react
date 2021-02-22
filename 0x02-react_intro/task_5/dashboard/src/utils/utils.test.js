924afe04f8c08df524e01ae544560b355d83f62f
zulsb <zulsb2093@gmail.com>
2021-02-22 01:21:54 -0500

    Add basic tests

--- 0x02-react_intro/task_5/dashboard/src/utils/utils.test.js
+++ 0x02-react_intro/task_5/dashboard/src/utils/utils.test.js
@@ -1,1 +1,17 @@
 import { getLatestNotification, getFullYear, getFooterCopy } from './utils'
+
+describe('Basic tests', () => {
+
+	test('FullYear', () => {
+		expect(getFullYear()).toBe(new Date().getFullYear());
+	});
+
+	test('LatestNotification', () => {
+		expect(getLatestNotification()).toBe('<strong>Urgent requirement</strong> - complete by EOD');
+	});
+
+	test('FooterCopy', () => {
+		expect(getFooterCopy(false)).toBe('Holberton School main dashboard')
+		expect(getFooterCopy(true)).toBe('Holberton School');
+	});
+});
