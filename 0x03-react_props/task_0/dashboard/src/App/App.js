c1b1f516ed5584562b24b1538898d554481d5a89
zulsb <zulsb2093@gmail.com>
2021-02-26 23:14:39 -0500

    Basic components

--- 0x03-react_props/task_0/dashboard/src/App/App.js
+++ 0x03-react_props/task_0/dashboard/src/App/App.js
@@ -1,26 +1,19 @@
-import logo from '../assets/HLogo.jpg';
-import './App.css';
-import { getFullYear, getFooterCopy } from '../utils/utils';
+import React from 'react';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>School dashboard</h1>
+    <React.Fragment>
+      <Notifications/>
+      <div className="App">
+        <Header/>
+        <Login/>
+        <Footer/>
       </div>
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
-        <label htmlFor="email">Email: </label>
-        <input type="email" id="email" />
-        <label htmlFor="password">Password: </label>
-        <input type="password" id="password" />
-        <button>OK</button>
-      </div>
-      <div className="App-footer">
-        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
-      </div>
-    </div>
+    </React.Fragment>
   );
 }
 
