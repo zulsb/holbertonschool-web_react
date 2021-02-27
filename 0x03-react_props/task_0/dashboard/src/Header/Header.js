b7a3019bed0e743ec7a2db0e4c6532b2de7e7598
zulsb <zulsb2093@gmail.com>
2021-02-26 23:51:06 -0500

    Fix tab

--- 0x03-react_props/task_0/dashboard/src/Header/Header.js
+++ 0x03-react_props/task_0/dashboard/src/Header/Header.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import logo from '../assets/HLogo.jpg';
-import './Header.css';
+import React from "react";
+import logo from "../assets/HLogo.jpg";
+import "./Header.css";
 
 function Header() {
-	return (
-		<header className="App-header">
-			<img src={logo} className="App-logo" alt="logo" />
-			<h1>School dashboard</h1>
-		</header>
-	);
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>School dashboard</h1>
+    </header>
+  );
 }
 
 export default Header;
