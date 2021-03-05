130c6ce202ab5916be4605039723a3a1341026b1
zulsb <zulsb2093@gmail.com>
2021-03-05 02:29:35 -0500

    Commence with class components

--- 0x04-React_component/task_0/dashboard/src/App/App.js
+++ 0x04-React_component/task_0/dashboard/src/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Notifications from '../Notifications/Notifications';
 import { getLatestNotification } from '../utils/utils';
 import Header from '../Header/Header';
@@ -7,24 +7,24 @@ import Footer from '../Footer/Footer';
 import CourseList from '../CourseList/CourseList';
 import PropTypes from 'prop-types';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     const listCourses = [
       { id: 1, name: 'ES6', credit: 60 },
       { id: 2, name: 'Webpack', credit: 20 },
-      { id: 3, name: 'React', credit: 40 },
+      { id: 3, name: 'React', credit: 40 }
     ];
 
     const listNotifications = [
       { id: 1, type: 'default', value: 'New course available' },
       { id: 2, type: 'urgent', value: 'New resume available' },
-      { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
+      { id: 3, type: 'urgent', html: { __html: getLatestNotification() } }
     ];
 
     return (
       <React.Fragment>
         <Notifications listNotifications={listNotifications} />
-        <div className='App'>
+        <div className="App">
           <Header />
           {this.props.isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
           <Footer />
@@ -35,11 +35,11 @@ class App extends React.Component {
 }
 
 App.propTypes = {
-  isLoggedIn: PropTypes.bool,
+  isLoggedIn: PropTypes.bool
 };
 
 App.defaultProps = {
-  isLoggedIn: false,
+  isLoggedIn: false
 };
 
 export default App;
