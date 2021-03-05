5118cace1f6f850de060b4030e32ade35b3fc1bf
zulsb <zulsb2093@gmail.com>
2021-03-05 02:22:33 -0500

    Commence with class components

--- 0x04-React_component/task_0/dashboard/src/App/App.js
+++ 0x04-React_component/task_0/dashboard/src/App/App.js
@@ -1,30 +1,30 @@
-import React from "react";
-import Notifications from "../Notifications/Notifications";
-import { getLatestNotification } from "../utils/utils";
-import Header from "../Header/Header";
-import Login from "../Login/Login";
-import Footer from "../Footer/Footer";
-import CourseList from "../CourseList/CourseList";
-import PropTypes from "prop-types";
+import React from 'react';
+import Notifications from '../Notifications/Notifications';
+import { getLatestNotification } from '../utils/utils';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import CourseList from '../CourseList/CourseList';
+import PropTypes from 'prop-types';
 
 class App extends React.Component {
   render() {
     const listCourses = [
-      { id: 1, name: "ES6", credit: 60 },
-      { id: 2, name: "Webpack", credit: 20 },
-      { id: 3, name: "React", credit: 40 },
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 },
     ];
 
     const listNotifications = [
-      { id: 1, type: "default", value: "New course available" },
-      { id: 2, type: "urgent", value: "New resume available" },
-      { id: 3, type: "urgent", html: { __html: getLatestNotification() } },
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+      { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
     ];
 
     return (
       <React.Fragment>
         <Notifications listNotifications={listNotifications} />
-        <div className="App">
+        <div className='App'>
           <Header />
           {this.props.isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
           <Footer />
