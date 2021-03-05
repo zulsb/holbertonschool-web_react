7b09eecca7da586e0d3853aa7ba88c7be218c784
zulsb <zulsb2093@gmail.com>
2021-03-05 02:01:22 -0500

    Copy files from previous task

---
+++ 0x04-React_component/task_0/dashboard/src/CourseList/CourseListRow.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import CourseListRow from './CourseListRow';
+
+describe('CourseListRow test', () => {
+  test('CourseListRow exist', () => {
+    expect(shallow(<CourseListRow textFirstCell='test' />).exists());
+  });
+
+  test('isHeader = True and with one th', () => {
+    const compo = shallow(<CourseListRow isHeader={true} textFirstCell='test' />);
+    expect(compo.find('th')).to.have.lengthOf(1);
+    expect(compo.find('th').props()).to.have.property('colSpan', '2');
+  });
+
+  test('isHeader = True and with two th', () => {
+    const compo = shallow(<CourseListRow isHeader={true} textFirstCell='test' textSecondCell='test' />);
+    expect(compo.find('th')).to.have.lengthOf(2);
+  });
+
+  test('isHeader = False and with two td', () => {
+    const compo = shallow(<CourseListRow isHeader={false} textFirstCell='test' textSecondCell='test' />);
+    expect(compo.find('td')).to.have.lengthOf(2);
+  });
+});
