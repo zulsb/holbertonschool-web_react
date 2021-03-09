fb880b758b0292f6d9e8c08aa0750b92c765ff04
zulsb <zulsb2093@gmail.com>
2021-03-09 01:06:39 -0500

    Copy files from previous task

---
+++ 0x04-React_component/task_3/dashboard/src/App/App.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import Notifications from '../Notifications/Notifications';
+import { getLatestNotification } from '../utils/utils';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import CourseList from '../CourseList/CourseList';
+import PropTypes from 'prop-types';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.taps = this.taps.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.taps);
+  }
+
+  taps(event) {
+    if (event.key === 'h' && event.ctrlKey) {
+      alert('Logging you out');
+      this.props.logOut();
+    }
+  }
+  render() {
+    const listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 },
+    ];
+
+    const listNotifications = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+      { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
+    ];
+
+    return (
+      <React.Fragment>
+        <Notifications listNotifications={listNotifications} />
+        <div className='App'>
+          <Header />
+          {this.props.isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+          <Footer />
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: (() => {})
+};
+
+export default App;
