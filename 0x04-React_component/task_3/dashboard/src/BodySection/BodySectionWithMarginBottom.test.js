ebef2bed9113e3f142bc1b69c2fe9dd5eaf789d8
zulsb <zulsb2093@gmail.com>
2021-03-09 01:46:02 -0500

    Test the new components

---
+++ 0x04-React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import BodySection from './BodySection';
+import BodySectionWithMarginBottom from './BodySectionWithMarginBottom';
+
+describe('BodySectionWithMarginBottom test', () => {
+  test('Render exist', () => {
+    expect(shallow(<BodySectionWithMarginBottom title='test title' />).exists());
+  });
+
+  test('BodySection', () => {
+    const wrapper = shallow(<BodySectionWithMarginBottom title='test title'><p>test children node</p></BodySectionWithMarginBottom>)
+    expect(wrapper.contains(<div className='bodySectionWithMargin' />));
+    expect(wrapper.children()).to.have.lengthOf(1);
+    expect(wrapper.find(BodySection)).to.have.lengthOf(1);
+    expect(wrapper.find(BodySection).children()).to.have.lengthOf(1);
+    expect(wrapper.find(BodySection).props().title).to.equal('test title');
+    expect(wrapper.find('p')).to.have.lengthOf(1);
+    expect(wrapper.find('p').text()).to.equal('test children node');
+  })
+});
