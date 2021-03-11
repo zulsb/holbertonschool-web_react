f8a54a617c8f65d16ffdc97a2b24f4a59056ef4e
zulsb <zulsb2093@gmail.com>
2021-03-10 22:23:24 -0500

    Copy files from previous task

---
+++ 0x05-React_inline_styling/task_1/dashboard/src/HOC/WithLogging.test.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { expect as e } from 'chai';
+import Login from '../Login/Login';
+import WithLogging from './WithLogging';
+
+describe('WithLogging test', () => {
+  test('console.log call / pure html', () => {
+    const HOC = WithLogging(() => <a></a>);
+    console.log = jest.fn();
+    const compo = mount(<HOC />);
+    expect(console.log).toHaveBeenCalled();
+    expect(console.log).toHaveBeenCalledWith('Component Component is mounted');
+    compo.unmount();
+    expect(console.log).toHaveBeenCalled();
+    expect(console.log).toHaveBeenCalledWith('Component Component is going to unmount');
+  });
+
+  test('console.log called / Login component. ', () => {
+    const HOC = WithLogging(Login);
+    console.log = jest.fn();
+    const compo = mount(<HOC />);
+    expect(console.log).toHaveBeenCalled();
+    expect(console.log).toHaveBeenCalledWith('Component Login is mounted');
+    compo.unmount();
+    expect(console.log).toHaveBeenCalled();
+    expect(console.log).toHaveBeenCalledWith('Component Login is going to unmount');
+  });
+});
