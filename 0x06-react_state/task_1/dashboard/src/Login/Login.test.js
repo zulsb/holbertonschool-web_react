31dbefb328a45894a66ad0cce05bde91e9343cc3
zulsb <zulsb2093@gmail.com>
2021-03-23 02:32:21 -0500

    Controlled components and state callback

---
+++ 0x06-react_state/task_1/dashboard/src/Login/Login.test.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { expect } from "chai";
+import App from "../App/App";
+import Login from "./Login";
+import { StyleSheetTestUtils } from "aphrodite";
+
+describe("Login test", () => {
+  beforeAll(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+
+  afterAll(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+
+  test("Login exist", (done) => {
+    expect(shallow(<Login />).exists());
+    done();
+  });
+
+  test("App-body", (done) => {
+    expect(shallow(<App />).contains(<body className="App-body" />));
+    done();
+  });
+
+  test("Inputs and labels", (done) => {
+    expect(shallow(<Login />).find("input")).to.have.lengthOf(2);
+    expect(shallow(<Login />).find("label")).to.have.lengthOf(2);
+    done();
+  });
+
+  test("Submit button = disabled by default", () => {
+    expect(shallow(<Login />).state().enableSubmit).to.equal(false);    
+  });
+
+  test("The button = enabled", () => {
+    const compo = shallow(<Login />);
+    compo.find("input#email").simulate("change", { target: { value: "test@test.com" } });
+    compo.find("input#password").simulate("change", { target: { value: "test" } });
+    expect(compo.state().enableSubmit).to.equal(true);
+  });
+});
